b85e3b5c02fd81e4363ed837e77a210e0005b278
Steve Fumarolo <steve.fumarolo@gmail.com>
2024-12-12 20:38:53 -0600

    Day 12 - Part 2

--- day12/day12.js
+++ day12/day12.js
@@ -3,14 +3,56 @@ const fs = require('fs');
 const grid = [];
 const areas = {};
 
+var countFullEdges = false;
+const gridEntry = {char : null, region : null, topEdge : false, bottomEdge : false, leftEdge : false, rightEdge : false};
+
+
 function readGrid(input) {
     const lines = input.trim().split('\n');
     for (const line of lines) {
         const row = [];
         for (const char of line) {
-            row.push({char : char, region : null});
+            let newGridEntry = {...gridEntry}
+            newGridEntry.char = char;
+            row.push(newGridEntry);
         }
         grid.push(row);
+    }
+}
+
+function findEdges() {
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            if (row === 0 || (grid[row - 1][col].region !== grid[row][col].region)) {
+                grid[row][col].topEdge = true;
+                if (col === 0 || (grid[row][col - 1].region !== grid[row][col].region || !grid[row][col - 1].topEdge)) {
+                    //console.log(grid[row][col]);
+                    areas[grid[row][col]["region"]].uniqueEdges++;
+                }
+            }
+
+            if (row === grid.length - 1 || (grid[row + 1][col].region !== grid[row][col].region)) {
+                grid[row][col].bottomEdge = true;
+                if (col === 0 || (grid[row][col - 1].region !== grid[row][col].region || !grid[row][col - 1].bottomEdge)) {
+                    areas[grid[row][col]["region"]].uniqueEdges++;
+                }
+
+            }
+
+            if (col === 0 || (grid[row][col - 1].region !== grid[row][col].region)) {
+                grid[row][col].leftEdge = true;
+                if (row === 0 || (grid[row - 1][col].region !== grid[row][col].region || !grid[row - 1][col].leftEdge)) {
+                    areas[grid[row][col]["region"]].uniqueEdges++;
+                }
+            }
+
+            if (col === grid[row].length - 1 || (grid[row][col + 1].region !== grid[row][col].region)) {
+                grid[row][col].rightEdge = true;
+                if (row === 0 || (grid[row - 1][col].region !== grid[row][col].region || !grid[row - 1][col].rightEdge)) {
+                    areas[grid[row][col]["region"]].uniqueEdges++;
+                }
+            }
+        }
     }
 }
 
@@ -31,7 +73,7 @@ function floodFill(row, col, char, region) {
 
     if (grid[row][col].char === char) {
         //let area = 1;
-        console.log(`Filling ${row}, ${col} with ${char} and region ${region}`);
+        // console.log(`Filling ${row}, ${col} with ${char} and region ${region}`);
         grid[row][col].region = region;
         const recursiveUp    = floodFill(row - 1, col, char, region);
         const recusiveRight  = floodFill(row + 1, col, char, region);
@@ -54,7 +96,7 @@ function labelRegions() {
                 const areaFilled = floodFill(row, col, grid[row][col].char, region);
                 if (areaFilled[0] > 0) {
                     console.log(`Region ${region} for char ${grid[row][col].char} has area ${areaFilled}`);
-                    areas[region] = areaFilled;
+                    areas[region] = {area : areaFilled[0], edges : areaFilled[1], uniqueEdges : 0};
                     region++;
                 }
                 //console.log(grid);
@@ -70,19 +112,30 @@ function part1(input) {
     readGrid(input);
     labelRegions();
     // console.log(grid);
-    console.log(areas);
+    //console.log(areas);
 
     let price = 0;
     for (const region in areas) {
-        price += areas[region][0] * areas[region][1];
+        price += areas[region]["area"] * areas[region]["edges"];
     }
     console.log(price);
 }
 
 function part2(input) {
     // Implement part 2 logic here
-    console.log("Running Part 2");
+    countFullEdges = true;
     console.log(input);
+    readGrid(input);
+    labelRegions();
+    findEdges();
+    // console.log(grid);
+    //console.log(areas);
+
+    let price = 0;
+    for (const region in areas) {
+        price += areas[region]["area"] * areas[region]["uniqueEdges"];
+    }
+    console.log(price);
 }
 
 function getInput(file) {
